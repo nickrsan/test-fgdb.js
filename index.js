8362f9eef285feba8f222683a6b0a64e0009b605
Nick Santos <nickrsan@users.noreply.github.com>
2016-01-26 16:35:30 -0800

    Updated locations

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function color(s){
 	        return colorbrewer.Spectral[11][Math.abs(JSON.stringify(s).split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0)) % 11];
         }
-var map = L.map('map').setView([ 42.3560069, -71.0458374], 11);
+var map = L.map('map').setView([ 38.3, -90.3], 11);
 
         L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',{attribution:'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(map);
 
-
-//(function(){
 		
 		var options = {
 			onEachFeature: function(feature, layer) {
@@ -34,6 +32,4 @@ var map = L.map('map').setView([ 42.3560069, -71.0458374], 11);
         }
 		};
 
-	L.fileGDB("./fgdb.gdb.zip", options).addTo(map)
-	//L.fileGDB("./valmeyer_full.gdb.zip", options).addTo(map)
-//})();
+	L.fileGDB("./valmeyer_full.gdb.zip", options).addTo(map)
